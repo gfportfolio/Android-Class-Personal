b537e5baf7267ad2558cd32c29e1ecca93c0a699
gfportfolio <gbfarns16@gmail.com>
2015-02-21 20:13:56 -0700

    made layout for main and add, added list

--- CanIbuyit/app/src/main/java/weeblycom/gfportfolio/gavinfarnsworth/canibuyit/MainActivity.java
+++ CanIbuyit/app/src/main/java/weeblycom/gfportfolio/gavinfarnsworth/canibuyit/MainActivity.java
@@ -1,19 +1,31 @@
 package weeblycom.gfportfolio.gavinfarnsworth.canibuyit;
 
+import android.content.Intent;
+import android.graphics.Outline;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends ActionBarActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 
+    public void addTransactionClick(View v){
+        Intent intent = new Intent(this,AddTransaction.class);
+        startActivity(intent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
