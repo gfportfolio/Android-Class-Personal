531177ea1fff36f73faaa3a37434b4bfc82cb24f
gfportfolio <gbfarns16@gmail.com>
2015-02-25 21:44:54 -0700

    modified to get lists working on the list view

--- CanIbuyit/app/src/main/java/weeblycom/gfportfolio/gavinfarnsworth/canibuyit/listactivity.java
+++ CanIbuyit/app/src/main/java/weeblycom/gfportfolio/gavinfarnsworth/canibuyit/listactivity.java
@@ -5,11 +5,19 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import weeblycom.gfportfolio.gavinfarnsworth.canibuyit.Accounts.Account;
+import weeblycom.gfportfolio.gavinfarnsworth.canibuyit.Transactions.Transaction;
 
 
-public class listactivity extends ListActivity {
+public class listactivity extends ActionBarActivity {
     public ListView listView;
 
     @Override
@@ -18,11 +26,32 @@ public class listactivity extends ListActivity {
         setContentView(R.layout.activity_listactivity);
         listView = (ListView) findViewById(R.id.list_listactivity);
 
-        AccountListAdapter adapter = new AccountListAdapter(this, Model.accountManager.getCurrentAccounts());
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        "Click ListItem Number " + position, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+        ArrayList<String> accounts = new ArrayList<String>() ;
+        ArrayList<String> transactions = new ArrayList<String>() ;
+        for(Account a : Model.accountManager.getCurrentAccounts()){
+            accounts.add(a.getName()+"\t\t\t$"+a.getCurrentBalance());
+        }
+        for(Transaction t: Model.transactionManager.getTransactionHistory()){
+            transactions.add(t.getName()+"\t\t\t$"+t.getCost());
+        }
+
+               ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1
+                , accounts);
         listView.setAdapter(adapter);
 
-
-
+//        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1
+//                , transactions);
+//        transactionsListView.setAdapter(adapter2);
 
     }
 
